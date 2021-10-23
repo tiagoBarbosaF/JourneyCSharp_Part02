3a2e22f4f3a5ff873e213840a2c61c2faf7ec44b
Tiago Barbosa <tiago@farias.cc>
2021-10-22 21:12:22 -0300

    Journey CSharp Part02 - Introduction to object orientation

---
+++ P01-ByteBank/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace P01_ByteBank
+{
+  internal class Program
+  {
+    public static void Main(string[] args)
+    {
+      ContaCorrente contaCorrente = new ContaCorrente();
+
+      contaCorrente.Titular = "Tiago Barbosa";
+      contaCorrente.Agencia = 897;
+      contaCorrente.Numero = 897325;
+      contaCorrente.Saldo = 10000.50;
+
+      Console.WriteLine("\nBanco ByteBank\n");
+      Console.WriteLine($"Titular: {contaCorrente.Titular}");
+      Console.WriteLine($"Agência: {contaCorrente.Agencia}");
+      Console.WriteLine($"Número: {contaCorrente.Numero}");
+      Console.WriteLine($"Saldo: {contaCorrente.Saldo:F2}");
+
+      contaCorrente.Saldo += 500.65;
+      Console.WriteLine($"\nNovo saldo: {contaCorrente.Saldo:F2}");
+      
+    }
+  }
+}
